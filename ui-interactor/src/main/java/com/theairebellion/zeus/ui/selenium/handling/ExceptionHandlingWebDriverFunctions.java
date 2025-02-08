b628356d3471c5334deab29144c7142b2335d0ac
CyborgBoromir <boromir@cyborgcode.io>
2025-02-08 11:06:59 +0100

    Exception handling initial implementation;

--- ui-interactor/src/main/java/com/theairebellion/zeus/ui/selenium/handling/ExceptionHandlingWebDriverFunctions.java
+++ ui-interactor/src/main/java/com/theairebellion/zeus/ui/selenium/handling/ExceptionHandlingWebDriverFunctions.java
@@ -10,9 +10,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class ExceptionHandlingWebDriverFunctions {
 
@@ -22,7 +20,7 @@ public class ExceptionHandlingWebDriverFunctions {
             throw new IllegalArgumentException("FIND_ELEMENT action requires a By locator.");
         }
 
-        WebElement foundElement = tryToFindElementInIFrame(driver, (By) args[0]);
+        WebElement foundElement = findElementInIFrames(driver, (By) args[0]);
         if (foundElement != null) {
             return webElementAction.performActionWebDriver(driver, args);
         }
@@ -31,44 +29,12 @@ public class ExceptionHandlingWebDriverFunctions {
         throw new NoSuchElementException("Element not found in any iframe.");
     }
 
-//    private static SmartWebElement tryToFindElementInIFrame(WebDriver driver, By by) {
-//        driver.switchTo().defaultContent();
-//        return findElementRecursively(driver, by);
-//
-//    }
-
-//    private static SmartWebElement findElementRecursively(WebDriver driver, By by) {
-//        try {
-//            WebElement element = new SmartWebDriver(driver).findElement(by);
-//            return new SmartWebElement(element, driver);
-//        } catch (NoSuchElementException e) {
-//        }
-//
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        List<WebElement> frames = (List<WebElement>) js.executeScript(
-//                "return Array.from(document.getElementsByTagName('iframe'));"
-//        );
-//
-//        for (WebElement frame : frames) {
-//            driver.switchTo().frame(frame);
-//
-//            SmartWebElement elementInChild = findElementRecursively(driver, by);
-//            if (elementInChild != null) {
-//                return new SmartWebElement(elementInChild, driver);
-//            }
-//
-//            driver.switchTo().parentFrame();
-//        }
-//
-//        return null;
-//    }
-
-    private static SmartWebElement tryToFindElementInIFrame(WebDriver driver, By by) {
+    private static SmartWebElement findElementInIFrames(WebDriver driver, By by) {
         driver.switchTo().defaultContent();
-        return findElementRecursively(driver, by, new HashSet<>());
+        return searchElementInIFrames(driver, by);
     }
 
-    private static SmartWebElement findElementRecursively(WebDriver driver, By by, Set<WebElement> visitedFrames) {
+    private static SmartWebElement searchElementInIFrames(WebDriver driver, By by) {
         try {
             WebElement element = new SmartWebDriver(driver).findElement(by);
             return new SmartWebElement(element, driver);
@@ -81,18 +47,13 @@ public class ExceptionHandlingWebDriverFunctions {
         );
 
         for (WebElement frame : frames) {
-            if (visitedFrames.contains(frame)) {
-                continue;
-            }
-
-            visitedFrames.add(frame);
             driver.switchTo().frame(frame);
-            SmartWebElement elementInChild = findElementRecursively(driver, by, visitedFrames);
-            if (elementInChild != null) {
-                return elementInChild;
+            try {
+                WebElement element = driver.findElement(by);
+                return new SmartWebElement(element, driver);
+            } catch (Exception e) {
+                driver.switchTo().defaultContent();
             }
-
-            driver.switchTo().parentFrame();
         }
 
         return null;
