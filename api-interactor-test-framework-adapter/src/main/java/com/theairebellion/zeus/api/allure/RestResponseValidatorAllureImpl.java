c88b2aa162c254f26e6a92445cbb9f59315b182e
CyborgBoromir <boromir@cyborgcode.io>
2025-02-13 17:53:46 +0200

    modified allure steps for Rest and DB;

--- api-interactor-test-framework-adapter/src/main/java/com/theairebellion/zeus/api/allure/RestResponseValidatorAllureImpl.java
+++ api-interactor-test-framework-adapter/src/main/java/com/theairebellion/zeus/api/allure/RestResponseValidatorAllureImpl.java
@@ -2,7 +2,6 @@ package com.theairebellion.zeus.api.allure;
 
 import com.theairebellion.zeus.api.validator.RestResponseValidatorImpl;
 import io.qameta.allure.Allure;
-import io.qameta.allure.Step;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -11,11 +10,19 @@ import java.util.Map;
 public class RestResponseValidatorAllureImpl extends RestResponseValidatorImpl {
 
     @Override
-    @Step("Validating response with {assertions.length} assertion(s)")
     protected void printAssertionTarget(final Map<String, Object> data) {
-        super.printAssertionTarget(data);
-        Allure.step("Collected data to be validated", () -> {
+        Allure.step("Validating response with " + data.size() + " assertion(s)", () -> {
+            super.printAssertionTarget(data);
             Allure.addAttachment("Data to be validated", data.toString());
+            if (data.containsKey("statusCode")) {
+                Allure.addAttachment("Expected Status Code", data.get("statusCode").toString());
+            }
+            if (data.containsKey("headers")) {
+                Allure.addAttachment("Expected Headers", data.get("headers").toString());
+            }
+            if (data.containsKey("body")) {
+                Allure.addAttachment("Expected Response Body", data.get("body").toString());
+            }
         });
     }
 
--- db-interactor-test-framework-adapter/src/main/java/com/theairebellion/zeus/db/allure/QueryResponseValidatorAllureImpl.java
+++ db-interactor-test-framework-adapter/src/main/java/com/theairebellion/zeus/db/allure/QueryResponseValidatorAllureImpl.java
@@ -3,7 +3,6 @@ package com.theairebellion.zeus.db.allure;
 import com.theairebellion.zeus.db.json.JsonPathExtractor;
 import com.theairebellion.zeus.db.validator.QueryResponseValidatorImpl;
 import io.qameta.allure.Allure;
-import io.qameta.allure.Step;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -17,12 +16,10 @@ public class QueryResponseValidatorAllureImpl extends QueryResponseValidatorImpl
 
 
     @Override
-    @Step("Validating response with {assertions.length} assertion(s)")
     protected void printAssertionTarget(final Map<String, Object> data) {
         super.printAssertionTarget(data);
-        Allure.step("Collected data to be validated", () -> {
-            Allure.addAttachment("Data to be validated", data.toString());
-        });
+        Allure.step("Validating query response with " + data.size() + " assertion(s)", () ->
+                Allure.addAttachment("Data to be validated", data.toString()));
     }
 
 }
