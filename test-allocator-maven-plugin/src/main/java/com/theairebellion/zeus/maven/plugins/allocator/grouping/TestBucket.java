6b638308f3515dd417e01c536a32b5906c2d46a8
CyborgLegolas <legolas@cyborgcode.io>
2025-03-12 14:18:26 +0100

    test

--- test-allocator-maven-plugin/src/main/java/com/theairebellion/zeus/maven/plugins/allocator/grouping/TestBucket.java
+++ test-allocator-maven-plugin/src/main/java/com/theairebellion/zeus/maven/plugins/allocator/grouping/TestBucket.java
@@ -5,11 +5,24 @@ import lombok.Getter;
 
 import java.util.List;
 
+/**
+ * Represents a bucket of test classes grouped together for parallel execution.
+ * <p>
+ * This class stores a list of test class names and the total number of test methods
+ * contained within those classes. It is primarily used by the {@code TestBucketAllocator}
+ * to efficiently distribute tests across parallel execution threads.
+ * </p>
+ *
+ * @author Cyborg Code Syndicate
+ */
 @AllArgsConstructor
 @Getter
 public class TestBucket {
 
+    /** List of fully qualified class names contained in this test bucket. */
     private final List<String> classNames;
+
+    /** Total number of test methods across all classes in this bucket. */
     private final int totalMethods;
 
 }
