180bd5ba215d456d5341c1a24f4fcd66d9ed77ab
CyborgLegolas <legolas@cyborgcode.io>
2025-04-03 17:43:49 +0200

    test

--- reqres-test-framework/src/test/java/com/reqres/test/framework/ReqresApiAIGeneratedTest.java
+++ reqres-test-framework/src/test/java/com/reqres/test/framework/ReqresApiAIGeneratedTest.java
@@ -1,18 +1,28 @@
 package com.reqres.test.framework;
 
+import com.reqres.test.framework.rest.authentication.AdminAuth;
+import com.reqres.test.framework.rest.authentication.ReqResAuthentication;
 import com.reqres.test.framework.rest.dto.request.LoginUser;
 import com.reqres.test.framework.rest.dto.request.User;
 import com.theairebellion.zeus.api.annotations.API;
+import com.theairebellion.zeus.api.annotations.AuthenticateViaApiAs;
+import com.theairebellion.zeus.api.storage.StorageKeysApi;
 import com.theairebellion.zeus.framework.annotation.Craft;
 import com.theairebellion.zeus.framework.annotation.Regression;
+import com.theairebellion.zeus.framework.annotation.Ripper;
+import com.theairebellion.zeus.framework.annotation.Smoke;
 import com.theairebellion.zeus.framework.base.BaseTest;
+import com.theairebellion.zeus.framework.parameters.Late;
 import com.theairebellion.zeus.framework.quest.Quest;
 import com.theairebellion.zeus.validator.core.Assertion;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static com.reqres.test.framework.base.World.OLYMPYS;
-import static com.reqres.test.framework.data.creator.TestDataCreator.LOGIN_ADMIN_USER;
-import static com.reqres.test.framework.data.creator.TestDataCreator.USER_LEADER;
+import static com.reqres.test.framework.data.cleaner.TestDataCleaner.DELETE_ADMIN_USER;
+import static com.reqres.test.framework.data.creator.TestDataCreator.*;
 import static com.reqres.test.framework.rest.ApiResponsesJsonPaths.*;
 import static com.reqres.test.framework.rest.Endpoints.*;
 import static com.reqres.test.framework.utils.Helpers.EMPTY_JSON;
@@ -22,6 +32,7 @@ import static com.reqres.test.framework.utils.TestConstants.FileConstants.AVATAR
 import static com.reqres.test.framework.utils.TestConstants.Login.TOKEN_REGEX;
 import static com.reqres.test.framework.utils.TestConstants.Pagination.PAGE_ONE;
 import static com.reqres.test.framework.utils.TestConstants.Pagination.PAGE_TWO;
+import static com.reqres.test.framework.utils.TestConstants.Roles.*;
 import static com.reqres.test.framework.utils.TestConstants.Support.SUPPORT_TEXT_PREFIX;
 import static com.reqres.test.framework.utils.TestConstants.Users.*;
 import static com.theairebellion.zeus.api.validator.RestAssertionTarget.*;
@@ -29,6 +40,7 @@ import static com.theairebellion.zeus.validator.core.AssertionTypes.*;
 import static io.restassured.http.ContentType.JSON;
 import static org.apache.http.HttpHeaders.CONTENT_TYPE;
 import static org.apache.http.HttpStatus.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @API
 public class ReqresApiAIGeneratedTest extends BaseTest {
@@ -111,6 +123,82 @@ public class ReqresApiAIGeneratedTest extends BaseTest {
                         GET_USER.withPathParam(ID_PARAM, INVALID_USER_ID),
                         Assertion.builder().target(STATUS).type(IS).expected(SC_NOT_FOUND).build(),
                         Assertion.builder().target(BODY).key(ROOT.getJsonPath()).type(IS).expected(EMPTY_JSON).build()
+                )
+                .complete();
+    }
+
+    @Test
+    @Smoke
+    @Regression
+    @AuthenticateViaApiAs(credentials = AdminAuth.class, type = ReqResAuthentication.class)
+    @Ripper(targets = {DELETE_ADMIN_USER})
+    public void testCreateAndDeleteLeaderUser(Quest quest, @Craft(model = USER_LEADER) User userLeader) {
+        quest.enters(OLYMPYS)
+                .requestAndValidate(
+                        POST_CREATE_USER,
+                        userLeader,
+                        Assertion.builder().target(STATUS).type(IS).expected(SC_CREATED).build(),
+                        Assertion.builder().target(BODY).key(CREATE_USER_NAME_RESPONSE.getJsonPath()).type(IS).expected(USER_LEADER_NAME).soft(true).build(),
+                        Assertion.builder().target(BODY).key(CREATE_USER_JOB_RESPONSE.getJsonPath()).type(IS).expected(USER_LEADER_JOB).soft(true).build()
+                )
+                .request(
+                        DELETE_USER.withPathParam(
+                                ID_PARAM,
+                                retrieve(StorageKeysApi.API, POST_CREATE_USER, Response.class)
+                                        .getBody()
+                                        .jsonPath()
+                                        .getString(CREATED_USER_ID.getJsonPath())
+                        )
+                )
+                .complete();
+    }
+
+    @Test
+    @Smoke
+    @Regression
+    @AuthenticateViaApiAs(credentials = AdminAuth.class, type = ReqResAuthentication.class)
+    @Ripper(targets = {DELETE_ADMIN_USER})
+    public void testFullUserLifecycleAndCrossValidation(
+            Quest quest,
+            @Craft(model = USER_LEADER) User userLeader,
+            @Craft(model = USER_JUNIOR) Late<User> userJunior
+    ) {
+        quest.enters(OLYMPYS)
+                .requestAndValidate(
+                        POST_CREATE_USER,
+                        userLeader,
+                        Assertion.builder().target(STATUS).type(IS).expected(SC_CREATED).build(),
+                        Assertion.builder().target(BODY).key(CREATE_USER_NAME_RESPONSE.getJsonPath()).type(IS).expected(USER_LEADER_NAME).soft(true).build(),
+                        Assertion.builder().target(BODY).key(CREATE_USER_JOB_RESPONSE.getJsonPath()).type(IS).expected(USER_LEADER_JOB).soft(true).build()
+                )
+                .requestAndValidate(
+                        POST_CREATE_USER,
+                        userJunior.join(),
+                        Assertion.builder().target(STATUS).type(IS).expected(SC_CREATED).build(),
+                        Assertion.builder().target(BODY).key(CREATE_USER_NAME_RESPONSE.getJsonPath()).type(IS).expected(USER_JUNIOR_NAME).soft(true).build(),
+                        Assertion.builder().target(BODY).key(CREATE_USER_JOB_RESPONSE.getJsonPath()).type(IS).expected(USER_JUNIOR_JOB).soft(true).build()
+                )
+
+                .request(GET_ALL_USERS.withQueryParam(PAGE_PARAM, PAGE_TWO))
+
+                .validate(() -> {
+                    List<String> emails = retrieve(StorageKeysApi.API, GET_ALL_USERS, Response.class)
+                            .getBody()
+                            .jsonPath()
+                            .getList(USER_EMAIL_BY_INDEX.getJsonPath().replace("[%d]", ""), String.class);
+
+                    assertTrue(
+                            emails.stream().noneMatch(email -> email.contains(USER_SENIOR_NAME.toLowerCase())),
+                            "Unexpected: Senior user email found in paginated list!"
+                    );
+                })
+
+                .request(
+                        DELETE_USER.withPathParam(
+                                ID_PARAM,
+                                retrieve(StorageKeysApi.API, POST_CREATE_USER, Response.class)
+                                        .getBody().jsonPath().getString(CREATED_USER_ID.getJsonPath())
+                        )
                 )
                 .complete();
     }
