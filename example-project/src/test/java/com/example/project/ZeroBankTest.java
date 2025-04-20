0dfc10722e9111a06f9021ef6a8168f3a6f5001d
CyborgGandalf <gandalf@cyborgcode.io>
2025-04-20 16:33:32 +0200

    all modules pass parent pom

--- example-project/src/test/java/com/example/project/ZeroBankTest.java
+++ example-project/src/test/java/com/example/project/ZeroBankTest.java
@@ -7,6 +7,7 @@ import com.example.project.ui.elements.ZeroBank.*;
 import com.theairebellion.zeus.framework.annotation.Regression;
 import com.theairebellion.zeus.framework.base.BaseTest;
 import com.theairebellion.zeus.framework.quest.Quest;
+import com.theairebellion.zeus.ui.annotations.InterceptRequests;
 import com.theairebellion.zeus.ui.annotations.UI;
 import com.theairebellion.zeus.ui.components.table.base.TableField;
 import com.theairebellion.zeus.validator.core.Assertion;
@@ -113,6 +114,7 @@ public class ZeroBankTest extends BaseTest {
     @Test
     @Description("COMPONENTS: Button, Input, Link, List, Validate, Select")
     @Regression
+    @InterceptRequests
     public void paragraphTextValueTestHardAssertions(Quest quest) {
         quest
                 .enters(World.EARTH)
---
+++ ui-interactor-test-framework-adapter/src/main/java/com/theairebellion/zeus/ui/util/ResponseFormatter.java
@@ -0,0 +1,5 @@
+package com.theairebellion.zeus.ui.util;
+
+public class RepsonseFormatter {
+
+}
