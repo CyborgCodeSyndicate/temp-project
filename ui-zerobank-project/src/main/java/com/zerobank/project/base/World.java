9c86c8b13946708beb984116871784b60a4497a9
CyborgGandalf <gandalf@cyborgcode.io>
2025-07-25 14:58:30 +0200

    fix job

--- ui-zerobank-project/src/main/java/com/zerobank/project/base/World.java
+++ ui-zerobank-project/src/main/java/com/zerobank/project/base/World.java
@@ -1,12 +1,10 @@
 package com.zerobank.project.base;
 
-import com.theairebellion.zeus.api.service.fluent.RestServiceFluent;
 import com.theairebellion.zeus.db.service.fluent.DatabaseServiceFluent;
 import com.zerobank.project.ui.UiServiceCustom;
 
 public class World {
 
-   public static final Class<RestServiceFluent> OLYMPYS = RestServiceFluent.class;
    public static final Class<DatabaseServiceFluent> UNDERWORLD = DatabaseServiceFluent.class;
    public static final Class<UiServiceCustom> EARTH = UiServiceCustom.class;
 }
